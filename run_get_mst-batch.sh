5b70230c91e9cb9b4624306265560a2c9edb2a61
Paul Aljabar <m.mouse@disney.com>
2015-06-18 13:29:45 +0100

    Choice of script to run for obtaining MST. Depends on type of connectivity matrix.

--- run_get_mst-batch.sh
+++ run_get_mst-batch.sh
@@ -16,13 +16,21 @@ idListFile="ids.txt"
 # Ignore lines in the file that start with a # character.
 commentPattern=' *\#'
 
+# For original connectivity matrices.
+SCRIPTA='run_get_mst-conn-raw.sh'
+# For ipsilateral-contralateral representations.
+SCRIPTB='run_get_mst-ipsi_contra.sh'
+
+# Choice!
+SCRIPT=$SCRIPTB
+
 while read id
 do
     if [[ "${id}" =~ ${commentPattern}  ]] ; then continue ; fi
 
     timeStamp=`date +'%Y%m%d-%H%M%S'`
     outputFile=${SCRIPTDIR}/output/output-mst-${timeStamp}-${id}.txt
-    command="run_get_mst.sh ${id} &>  ${outputFile}"
+    command="${SCRIPT} ${id} &>  ${outputFile}"
     echo ${command}
     eval ${command}
 
