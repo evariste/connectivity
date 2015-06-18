ce6cc98f1672b066094961e904da4f16e6002848
evariste <paul.aljabar@gmail.com>
2015-06-18 13:16:38 +0100

    Script for MST extraction from ipsilateral-contralateral representations of connectivity.

---
+++ run_get_mst-ipsi_contra.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+## USAGE:
+min_num_args=1
+if [ ! $# -ge $min_num_args ]; then
+  echo "  Usage $0 [ID]
+
+   Get Minimum spanning trees for ipsi-contra representations of connectivty.
+   Assumes a 90 region AAL labeling was used for tractography.
+"
+  exit
+fi
+
+
+source /projects/perinatal/peridata/paul/packages/python/envPA/bin/activate
+
+export PYTHONPATH=/projects/perinatal/peridata/paul/scripts/python_scripts:${PYTHONPATH}
+
+SCRIPTDIR="/projects/perinatal/peridata/paul/tractography/connectivity"
+DATADIR="/projects/perinatal/peridata/EPRIME"
+WORKDIR="/projects/perinatal/peridata/paul/EPRIME"
+
+
+id=$1
+
+
+# Where are the tractography results?
+tractDir=${WORKDIR}/${id}/001_diffusion_data/tractography/tracts
+
+
+labelNames="unc-aal unc-aal-with-subcort"
+
+# Take two components from the ipsi-contra matrices as the two types
+# of edges are in distinct sub-graphs. The minimum spanning tree will
+# then be obtained for each sub-graph
+
+
+
+for labelSet in $labelNames 
+do 
+    currDir=${tractDir}/${labelSet}
+
+    dataFile=${currDir}/anisotropy/ipsi-con.npy
+    mstFile=${currDir}/anisotropy/mst-ipsi-con.npy
+
+    command="python conn_mat_to_mst.py ${dataFile} ${mstFile} -comps 2"
+    echo ${command}
+    eval ${command}
+    echo
+
+    dataFile=${currDir}/probability/ipsi-con.npy
+    mstFile=${currDir}/probability/mst-ipsi-con.npy
+
+    command="python conn_mat_to_mst.py ${dataFile} ${mstFile} -comps 2"
+    echo ${command}
+    eval ${command}
+    echo
+
+done
+
+
+
+
+
